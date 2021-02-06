ad5afdd2ba60ae940da83fffb003397b6e133119
DaCurse <dacurse0@gmail.com>
2021-02-06 21:21:07 +0200

    Added tests for App, FormikRadioGroup

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,8 +2,6 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import App from '.';
 
-test('renders hello world message', () => {
-  const { getByText } = render(<App />);
-
-  expect(getByText(/Hello, World/)).toBeInTheDocument();
+it('renders without crashing', () => {
+  render(<App />);
 });
---
+++ src/components/Formik/FormikRadioGroup/FormikRadioGroup.test.tsx
@@ -0,0 +1,33 @@
+import { Button, Radio } from '@material-ui/core';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Form, Formik } from 'formik';
+import React from 'react';
+import FormikRadioGroup from '.';
+
+it('submits the form with the selected value', async () => {
+  const handleSubmit = jest.fn();
+  const initialValues = { test: '' };
+  const expectedValues = { test: 'bar ' };
+
+  render(
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {() => (
+        <Form>
+          <FormikRadioGroup name="test">
+            <Radio value="foo" />
+            <Radio value="bar" />
+            <Radio value="baz" />
+          </FormikRadioGroup>
+          <Button type="submit">Submit</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+
+  userEvent.click(screen.getByDisplayValue('bar'));
+  userEvent.click(screen.getByRole('button'));
+
+  await waitFor(() => expect(handleSubmit).toHaveBeenCalledTimes(1));
+  expect(handleSubmit).toHaveBeenCalledWith(expectedValues, expect.anything());
+});
